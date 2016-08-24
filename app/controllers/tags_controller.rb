d021f9fd159af98580cfbf84ff289ecf395b52ee
dowdje <dowdje89@gmail.com>
2016-08-23 23:10:52 -0400

    Blog Nested Forms

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,6 +6,16 @@ class TagsController < ApplicationController
     @tags = Tag.all
   end
 
+  def create
+    @tag = Tag.new(params.require(:post).permit(tags_attributes: [:name]))
+    if @tag.save
+      redirect_to post_path(params[:post])
+    else
+      redirect_to new_post_path
+    end
+  end
+
+
   # GET /tags/1
   # GET /tags/1.json
   def show
